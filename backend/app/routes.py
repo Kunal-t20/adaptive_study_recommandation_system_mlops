ea08062612d36bcfda862d39a42e688b0ae8e31b
KJM <kunalmahajan0202@gmail.com>
2026-04-19 13:33:29 +0530

    feedback loop added

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,26 +1,33 @@
 from fastapi import APIRouter
-from app.schemas import StudentInput,FeedbackInput
+from app.schemas import StudentInput, FeedbackInput
 import os
+import pandas as pd
 from services.prediction_service import PredictionService
 
 service = PredictionService()
+router = APIRouter()
 
-router=APIRouter()
-
+# ------------------ PREDICT ------------------
 @router.post('/predict')
-def predict(data:StudentInput):
-    result=service.predict(data.dict())
+def predict(data: StudentInput):
+    result = service.predict(data.dict())
+    return {"Prediction": result}
 
-    return {"Prediction":result}
 
-
+# ------------------ FEEDBACK ------------------
 @router.post("/feedback")
 def feedback(data: FeedbackInput):
-    file_path = "data/feedback.csv"
 
+    # correct path (important)
+    file_path = os.path.join("data", "feedback.csv")
+
+    # convert to dataframe
     df = pd.DataFrame([data.dict()])
 
-    # Check if file exists
+    # ensure folder exists
+    os.makedirs("data", exist_ok=True)
+
+    # save data
     if not os.path.exists(file_path):
         df.to_csv(file_path, index=False)
     else:
